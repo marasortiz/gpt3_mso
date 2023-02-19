9ec59727fdf2888240e44a37c1e736de9c51eac1
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-19 13:39:46 +0100

    Start of WhatGPT3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './whatGPT3.css';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-features'>
+        <Feature />
+        
+      </div>
+
+      {/* <h1>What is GPT-3</h1>
+      <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
+      <div>
+        <h1>The possibilities are beyond your imagination</h1>
+        <a href="#">Explore The Library</a>
+      </div>
+      <div>
+        <div>
+          <h2>Chatbots</h2>
+          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
+        </div>
+        <div>
+          <h2>Knowledgebase</h2>
+          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+        </div>
+        <div>
+          <h2>Education</h2>
+          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
+        </div>
+      </div> */}
+    </div>
   )
 }
 
