6221a8168aadecc912de3a44ef6a99a5e30879bd
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-20 12:41:10 +0100

    WhatGPT3 & Features Ended + Responsive

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import './whatGPT3.css';
-import { Feature } from '../../components';
+import React from "react";
+import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
-      <div className='gpt3__whatgpt3-features'>
-        <Feature />
-        
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-features">
+        <Feature
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
       </div>
-
-      {/* <h1>What is GPT-3</h1>
-      <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p>
-      <div>
-        <h1>The possibilities are beyond your imagination</h1>
-        <a href="#">Explore The Library</a>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
       </div>
-      <div>
-        <div>
-          <h2>Chatbots</h2>
-          <p>We so opinion friends me message as delight. Whole front do of plate heard oh ought. </p>
-        </div>
-        <div>
-          <h2>Knowledgebase</h2>
-          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
-        </div>
-        <div>
-          <h2>Education</h2>
-          <p>At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b</p>
-        </div>
-      </div> */}
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhatGPT3
+export default WhatGPT3;
