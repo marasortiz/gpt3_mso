d3840a462e45ebad1328c2609120f0fa60f2171c
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-21 16:56:32 +0100

    Updates

---
+++ precache-manifest.b0a81b7e6debfb80c32ca4c3b5f18914.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f00944ec0eb13b8d60c4",
+    "url": "/marasortiz/gpt3_mso/static/css/main.cb5225d0.chunk.css"
+  },
+  {
+    "revision": "f00944ec0eb13b8d60c4",
+    "url": "/marasortiz/gpt3_mso/static/js/main.f00944ec.chunk.js"
+  },
+  {
+    "revision": "4824bc4903f26c2d73f7",
+    "url": "/marasortiz/gpt3_mso/static/js/1.4824bc49.chunk.js"
+  },
+  {
+    "revision": "76229534fe941781d3ac",
+    "url": "/marasortiz/gpt3_mso/static/js/runtime~main.76229534.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/marasortiz/gpt3_mso/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/marasortiz/gpt3_mso/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/marasortiz/gpt3_mso/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/marasortiz/gpt3_mso/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/marasortiz/gpt3_mso/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/marasortiz/gpt3_mso/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/marasortiz/gpt3_mso/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/marasortiz/gpt3_mso/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/marasortiz/gpt3_mso/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "61178279cc5e3a348fc910818058f2e5",
+    "url": "/marasortiz/gpt3_mso/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/marasortiz/gpt3_mso/precache-manifest.b0a81b7e6debfb80c32ca4c3b5f18914.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/marasortiz/gpt3_mso/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
