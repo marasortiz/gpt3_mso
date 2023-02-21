5873bafa61c898336e38f2ac2a96180d91bb5d0a
Mara <santamaria.ortiz.tamara@gmail.com>
2023-02-21 17:14:00 +0100

    Updates

---
+++ precache-manifest.2b53989c890247a027259761b626753c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f00944ec0eb13b8d60c4",
+    "url": "/gpt3_mso/static/css/main.cb5225d0.chunk.css"
+  },
+  {
+    "revision": "f00944ec0eb13b8d60c4",
+    "url": "/gpt3_mso/static/js/main.f00944ec.chunk.js"
+  },
+  {
+    "revision": "4824bc4903f26c2d73f7",
+    "url": "/gpt3_mso/static/js/1.4824bc49.chunk.js"
+  },
+  {
+    "revision": "1f78623761ad5ccdf639",
+    "url": "/gpt3_mso/static/js/runtime~main.1f786237.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/gpt3_mso/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/gpt3_mso/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/gpt3_mso/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/gpt3_mso/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/gpt3_mso/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/gpt3_mso/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/gpt3_mso/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/gpt3_mso/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/gpt3_mso/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "c983591b812bca93b5284d7625d423de",
+    "url": "/gpt3_mso/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/marasortiz/gpt3_mso/precache-manifest.b0a81b7e6debfb80c32ca4c3b5f18914.js"
+  "/gpt3_mso/precache-manifest.2b53989c890247a027259761b626753c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/marasortiz/gpt3_mso/index.html", {
+workbox.routing.registerNavigationRoute("/gpt3_mso/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
